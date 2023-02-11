a69bce09912b79f6c924723b9ebc2a9d1b9e8157
Ev00gesha <rakzena180@gmail.com>
2023-02-11 22:44:19 +0300

    added a function that does not add a new cell if no move is made

--- app/src/main/java/spintowin2048/game/MainActivity.kt
+++ app/src/main/java/spintowin2048/game/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.core.content.ContextCompat
 import kotlin.random.Random
 import spintowin2048.game.databinding.ActivityMainBinding
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.concurrent.schedule
 import kotlin.math.abs
 import kotlin.math.max
@@ -26,18 +27,18 @@ class MainActivity : AppCompatActivity() {
     private val size = 4
     private var turn = 30
     private var score = 0
-    private var X = 0
-    private var Y = 0
-    private lateinit var currentRotation: CurrentRotation
+
+    private var startRotation = Rotation()
+    private var board = Array(size) { ArrayList<CellState>(size)}
+
+    private lateinit var currentRotation: Rotation
     private lateinit var config: Config
-    private var startRotation = StartRotation(null, null)
-
-    private var board = Array(size) {IntArray(size)}
-
     private lateinit var manager: SensorManager
     private lateinit var binding: ActivityMainBinding
     private lateinit var inflater: LayoutInflater
     private lateinit var cellAnimation: Animation
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,71 +50,145 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        initBoard()
         initTimer()
         addNumbers()
         showBoard()
         sensorListener()
     }
 
+    private fun initBoard(){
+        for (i in 0 until size) {
+            for(j in 0 until size){
+                board[i].add(CellState())
+            }
+        }
+    }
+
     private fun initTimer(){
-        val timer = Timer("schedule", true);
+        val timer = Timer("schedule", true)
 
         timer.schedule(1000, 700) {
             runOnUiThread { goToMove() }
         }
     }
 
+    private fun sensorListener(){
+        val sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        val listener = object: SensorEventListener{
+            override fun onSensorChanged(event: SensorEvent?) {
+                val x = ((event?.values!![0] * 90) / 9.81).toInt()
+                val y = ((event.values!![1] * 90) / 9.81).toInt()
+                val z = ((event.values!![2] * 90) / 9.81).toInt()
+
+                if(startRotation.x == null && startRotation.y == null && startRotation.z == null){
+                    startRotation.x = x
+                    startRotation.y = y
+                    startRotation.z = z
+                }
+
+                currentRotation = Rotation(x - startRotation.x!!, y - startRotation.y!!, z - startRotation.z!!)
+            }
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) { }
+        }
+        manager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
     private fun goToMove(){
-        when(max(abs(currentRotation.x), abs(max(abs(currentRotation.y), abs(currentRotation.z))))){
-            abs(currentRotation.x) -> {
-                if(currentRotation.x > turn){
+        when(max(abs(currentRotation.x!!), abs(max(abs(currentRotation.y!!), abs(currentRotation.z!!))))){
+            abs(currentRotation.x!!) -> {
+                if(currentRotation.x!! > turn){
                     moveCell("left")
                 }
-                if(currentRotation.x < turn * -1){
+                if(currentRotation.x!! < turn * -1){
                     moveCell("right")
                 }
             }
-            abs(currentRotation.y) -> {
-                if(currentRotation.y < turn * -1) {
+            abs(currentRotation.y!!) -> {
+                if(currentRotation.y!! < turn * -1) {
                     moveCell("up")
                 }
             }
-            abs(currentRotation.z) -> {
-                if(currentRotation.z < turn * -1) {
+            abs(currentRotation.z!!) -> {
+                if(currentRotation.z!! < turn * -1) {
                     moveCell("down")
                 }
             }
         }
     }
 
-    private fun getRowWithoutZero(row: Int): ArrayList<Int>{
-        var list = ArrayList<Int>()
+    private fun copyMatrix(before: Array<ArrayList<CellState>>){
+        for (i in 0 until size) {
+            for (j in 0 until size) {
+                before[i].add(board[i][j])
+            }
+        }
+    }
+
+    private fun moveCell(direction: String){
+        val beforeMerge = Array(size) { ArrayList<CellState>(size)}
+        copyMatrix(beforeMerge)
+
+        for(i in 0 until size){
+            if(direction == "left" || direction == "right"){
+                val rowWithout = getRowWithoutZero(i)
+                if(direction == "right")
+                    rowWithout.reverse()
+
+                val rowMerge = mergeCell(rowWithout)
+                when(direction){
+                    "left" -> replaceRow(i, 0, rowMerge)
+                    "right" -> replaceRow(i, size - 1, rowMerge)
+                }
+            }
+
+            if(direction == "up" || direction == "down"){
+                val columnWithout = getColumnWithoutZero(i)
+                if(direction == "down")
+                    columnWithout.reverse()
+
+                val columnMerge = mergeCell(columnWithout)
+                when(direction){
+                    "up" -> replaceColumn(i, 0, columnMerge)
+                    "down" -> replaceColumn(i, size - 1, columnMerge)
+                }
+            }
+        }
+        if(!beforeMerge.contentDeepEquals(board))
+            addNumbers()
+        showBoard()
+    }
+
+    private fun getRowWithoutZero(row: Int): ArrayList<CellState>{
+        val list = ArrayList<CellState>()
         for(j in 0 until size){
-            if(board[row][j] > 0) {
+            if(board[row][j].point > 0) {
                 list.add(board[row][j])
-                board[row][j] = 0
+                board[row][j] = CellState()
             }
         }
         return list
     }
 
-    private fun getColumnWithoutZero(column: Int): ArrayList<Int>{
-        var list = ArrayList<Int>()
+    private fun getColumnWithoutZero(column: Int): ArrayList<CellState>{
+        val list = ArrayList<CellState>()
         for(j in 0 until size){
-            if(board[j][column] > 0) {
+            if(board[j][column].point > 0) {
                 list.add(board[j][column])
-                board[j][column] = 0
+                board[j][column] = CellState()
             }
         }
         return list
     }
 
-    private fun mergeCell(cells: ArrayList<Int>): ArrayList<Int>{
+    private fun mergeCell(cells: ArrayList<CellState>): ArrayList<CellState>{
         var j = 0
         while(j < cells.size - 1) {
-            if (cells[j] == cells[j + 1]) {
-                cells[j] *= 2
-                score += cells[j]
+            if (cells[j].point == cells[j + 1].point) {
+                cells[j].point *= 2
+                cells[j].merge = true
+                score += cells[j].point
                 cells.removeAt(j + 1)
             }
             j++
@@ -121,57 +196,16 @@ class MainActivity : AppCompatActivity() {
         return cells
     }
 
-    private fun moveCell(direction: String){
-        when(direction){
-            "left" -> {
-                for (i in 0 until size) {
-                    var row = getRowWithoutZero(i)
-
-                    row = mergeCell(row)
-
-                    for(k in 0 until row.size) {
-                        board[i][k] = row[k]
-                    }
-                }
-            }
-            "right" -> {
-                for (i in 0 until size) {
-                    var row = getRowWithoutZero(i)
-                    row.reverse()
-
-                    row = mergeCell(row)
-
-                    for(k in 0 until row.size) {
-                        board[i][size - k - 1] = row[k]
-                    }
-                }
-            }
-            "up" -> {
-                for (i in 0 until size) {
-                    var column = getColumnWithoutZero(i)
-
-                    column = mergeCell(column)
-
-                    for (k in 0 until column.size){
-                        board[k][i] = column[k]
-                    }
-                }
-            }
-            "down" -> {
-                for (i in 0 until size) {
-                    var column = getColumnWithoutZero(i)
-                    column.reverse()
-
-                    column = mergeCell(column)
-
-                    for (k in 0 until column.size){
-                        board[size - k - 1][i] = column[k]
-                    }
-                }
-            }
+    private fun replaceRow(i: Int, n: Int, row: ArrayList<CellState>){
+        for(k in 0 until row.size){
+            board[i][abs(n - k)] = row[k]
         }
-        addNumbers()
-        showBoard()
+    }
+
+    private fun replaceColumn(i: Int, n: Int, column: ArrayList<CellState>){
+        for(k in 0 until column.size){
+            board[abs(n - k)][i] = column[k]
+        }
     }
 
     private fun addNumbers(){
@@ -184,55 +218,28 @@ class MainActivity : AppCompatActivity() {
                 x = place / 4
                 y = place % 4
             } while (!isEmptyCell(x, y))
-            X = x
-            Y = y
-            board[x][y] = arrayOf(2, 2, 2, 2, 2, 2, 2, 2, 2, 4).random()
+            board[x][y].point = arrayOf(2, 2, 2, 2, 2, 2, 2, 2, 2, 4).random()
+            board[x][y].merge = true
         }else{
             showToast("Ходов больше нет")
         }
     }
 
     private fun isEmptyCell(x: Int, y: Int):Boolean{
-        return board[x][y] == 0
+        return board[x][y].point == 0
     }
 
     private fun isEmptyBoard():Boolean{
         for (i in 0 until size)
             for(j in 0 until size)
-                if(board[i][j] == 0)
+                if(board[i][j].point == 0)
                     return true
         return false
-    }
-
-    private fun sensorListener(){
-        val sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-
-        val listener = object: SensorEventListener{
-            override fun onSensorChanged(event: SensorEvent?) {
-                var x = ((event?.values!![0] * 90) / 9.81).toInt()
-                var y = ((event?.values!![1] * 90) / 9.81).toInt()
-                var z = ((event?.values!![2] * 90) / 9.81).toInt()
-
-                if(startRotation.x == null && startRotation.y == null && startRotation.z == null){
-                    startRotation.x = x
-                    startRotation.y = y
-                    startRotation.z = z
-                }
-
-                currentRotation = CurrentRotation(x - startRotation.x!!, y - startRotation.y!!, z - startRotation.z!!)
-            }
-            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) { }
-        }
-        manager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     private fun showToast(msg: String){
         Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
     }
-
-    data class StartRotation(var x: Int? = null, var y: Int? = null, var z: Int? = null)
-
-    data class CurrentRotation(var x: Int, var y: Int, var z: Int)
 
     private fun showBoard(){
         binding.board.removeAllViews()
@@ -244,23 +251,27 @@ class MainActivity : AppCompatActivity() {
                 val cell = view.findViewById<CardView>(R.id.cell)
                 val points = view.findViewById<TextView>(R.id.points)
 
-                if(i == X && j == Y)
+                if(board[i][j].merge)
                     cell.startAnimation(cellAnimation)
-
-                if(board[i][j] == 0){
+                board[i][j].merge=false
+                if(board[i][j].point == 0){
                     points.text = ""
                     cell.setCardBackgroundColor(ContextCompat.getColor(this, R.color.white))
                 }
                 else{
-                    if(board[i][j] > 4)
+                    if(board[i][j].point > 4)
                         points.setTextColor(ContextCompat.getColor(this, R.color.white))
-                    points.text = board[i][j].toString()
-                    cell.setCardBackgroundColor(Color.parseColor(config.colors[board[i][j]]))
+                    points.text = board[i][j].point.toString()
+                    cell.setCardBackgroundColor(Color.parseColor(config.colors[board[i][j].point]))
                 }
 
-                binding.board!!.addView(view, binding.board!!.childCount)
+                binding.board.addView(view, binding.board.childCount)
             }
         }
     }
+
+    data class CellState(var point: Int = 0, var merge: Boolean = false)
+
+    data class Rotation(var x: Int? = null, var y: Int? = null, var z: Int? = null)
 }
 
